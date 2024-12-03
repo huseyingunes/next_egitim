be83d7e0b7a87d0472cb89801f68d4419b3b3906
Hüseyin GÜNEŞ <huseyingunes@gmail.com>
2024-12-03 13:59:35 +0300

    deisiklikler

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana, Rubik_Puddles } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ["400", "700"] });
+export const rubik_puddles = Rubik_Puddles({ subsets: ['latin'], weight: ["400"] });
